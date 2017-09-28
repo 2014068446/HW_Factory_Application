8fe79b426ba3542933d9a587f85e222594a4b956
2014068446 <2014068446@ust-ics.mygbiz.com>
2017-09-28 19:39:22 +0800

    fixed bug

--- HW_Factory_Application/src/com/controller/ProcessThang.java
+++ HW_Factory_Application/src/com/controller/ProcessThang.java
@@ -1,6 +1,8 @@
 package com.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -18,9 +20,16 @@ public class ProcessThang extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	
+		String phone = request.getParameter("phone");
+		PrintWriter out = response.getWriter();
+		out.println(phone);
+		Phone yPhone = new PhoneFactory().getPhone(phone);
+		Specs ySpecs = new SpecFactory().getSpecs(phone);
+		
+		yPhone.setSpecs(ySpecs);
+		
+		request.setAttribute("phoneData", yPhone);
 		request.getRequestDispatcher("display.jsp").forward(request, response);
-		System.out.print("gago");
 	}
 
 }
